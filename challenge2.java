ccb32045fcc4c23275f701d886c7b5a2a1e5d136
Meghana M R <86944692+mrmeghana@users.noreply.github.com>
2022-09-13 15:54:46 +0530

    Create challenge2.java

---
+++ challenge2.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        System.out.println(a.substring(m,n));
+    }
+}
